933f79636605f1b45f9cff34a58c1f88267011d4
Eshwara Chock <eshchock1@gmail.com>
2024-08-28 01:40:40 -0700

    no more name input

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,18 +1,3 @@
-function checkUserName() {//called when the user sumbits a username
-    userName = document.getElementById("userName").value;
-    if (userName.length > 0 && userName.length <= 18) {//making sure the username is valid (1 < length < 19)
-        //closing username menu
-        document.getElementById("userNameContainer").style.height = "0px";
-        document.getElementById("userNameContainer").style.opacity = 0;
-        //opening dino selection menu
-        document.getElementById("chooseDinoContainer").style.height = "500px";
-        document.getElementById("gameSubtitle").innerHTML = "CHOOSE YOUR DINO, " + userName.toUpperCase();
-    } else {//called if the name is invalid
-        document.getElementById("incorrectUsername").innerHTML = "Enter a valid username between 1 and 18 letters";
-        document.getElementById("userName").value = "";
-    }
-}
-
 function openHowToPlayMenu(){
   document.getElementById("howToPlayMenu").style.opacity = "1";
   document.getElementById("howToPlayMenu").style.zIndex = "10";
